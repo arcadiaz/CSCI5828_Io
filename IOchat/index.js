6c1224fac5e90febe305b1d53cda92735f594dbd
Arcadia Zhang <arcadia@rl1-1-139-218-edu.int.colorado.edu>
2018-04-06 14:37:45 -0600

    really made stylesheet link properly

--- IOchat/index.js
+++ IOchat/index.js
@@ -1,21 +1,30 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var tmi = require('tmi.js');
 var io = require('socket.io')(http);
+var path = require('path');
+
 
 
 var options = {
   identity:{
       username: "busunkim96",
-      password: "oauth:"
+      password: "oauth:gzx3qj2b3ibfalri7cn7zg0qpisllk"
   },
     channels: ["#arcadiiaaa"]
 };
+
+app.use(express.static(path.join(__dirname, '/public')));
+
 
 //send a file to the server
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
+
+
+
 
 http.listen(3000, function(){
     console.log('listening on *:3000');
@@ -33,7 +42,7 @@ client.addListener('join', function () {
 client.addListener('chat', function (channel, userstate, message, self) {
     console.log(message);
     var msg = userstate.username + ": " + message;
-    io.emit('chat message', msg);
+    io.emit('display messages', msg);
 });
 
 io.on('connection', function(socket){
