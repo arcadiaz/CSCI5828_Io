3d05a3436056a86cc1b7e76630abdd30ebc92fdb
busunkim96 <busunkim96@gmail.com>
2018-04-23 16:04:56 -0600

    updating payload

--- IOchat_python/server.py
+++ IOchat_python/server.py
@@ -1,6 +1,11 @@
 import requests
 
-payload = {'Emotes': 500}
+payload = {
+            'most_frequent_emote': 'some emote',
+            'most_frequent_phrase': 'some phrase',
+            'top_5_emotes': ['one', 'two', 'three', 'four', 'five'],
+            'text_summary': 'this is a brief summary of what happened'
+            }
 r = requests.post("http://localhost:3000/update", data=payload)
 print(r.text)
 
