8e8bdc14696fd3d8df3db696e555ae83c84f2658
Bu Sun Kim <busunkim96@gmail.com>
2018-04-25 19:16:23 -0600

    Update server.py

--- IOchat_python/server.py
+++ IOchat_python/server.py
@@ -1,15 +1,20 @@
 import requests
 from tools import Database, Stats
 import secret
+from flask import Flask
+import json 
 
-# payload = {
-#             'most_frequent_emote': 'some emote',
-#             'most_frequent_phrase': 'some phrase',
-#             'top_5_emotes': ['one', 'two', 'three', 'four', 'five'],
-#             'text_summary': 'this is a brief summary of what happened'
-#             }
-# r = requests.post("http://localhost:3000/update", data=payload)
-# print(r.text)
+app = Flask(__name__)
+
+@app.route('/getStats/<username>/<channel>', methods=['GET'])
+def get_stats(username=None, channel=None):
+	payload = {
+            'most_frequent_emote': 'some emote',
+            'most_frequent_phrase': 'some phrase',
+            'top_5_emotes': ['one', 'two', 'three', 'four', 'five'],
+            'text_summary': 'this is a brief summary of what happened'
+            }
+	return json.dumps(payload)
 
 DB = Database()
 # a = DB.retrieve_messages_time('test', '#test', '2018-04-23 00:00:37', 5)
