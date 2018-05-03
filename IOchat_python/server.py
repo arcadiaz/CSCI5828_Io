87c209a934cadd4b3b4d09535e8bfa0f357190c7
Bu Sun Kim <busunkim96@gmail.con>
2018-05-02 21:45:18 -0600

    setting up flask app to call tools methods based on query parameters

--- IOchat_python/server.py
+++ IOchat_python/server.py
@@ -1,9 +1,10 @@
 import requests
-# from tools import Database, Stats
-# import secret
+from tools import Database, Stats
+import secret
 from flask import Flask
 from flask_cors import CORS, cross_origin
 import json 
+import datetime
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -15,21 +16,31 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 $ export FLASK_APP=server.py
 $ python -m flask run
  * Running on http://127.0.0.1:5000/
+
+
 '''
+
+
+
+
 
 @app.route('/getStats/<username>/<channel>', methods=['GET'])
 @cross_origin()
 def get_stats(username=None, channel=None):
-	print(username, channel)
+
+
+	DB = Database()
+	now = datetime.datetime.now().strftime("%Y-%m-%d  %H:%M:%S")
+	a = DB.retrieve_messages_time(username, '#' + channel, now, 5000) 
+	top_emotes = Stats.get_top_emotes(a, ['hi', 'are', 'you'], 2)
+	phrases, n = Stats.get_top_phrases(a, ['no'], 5)
+	print(phrases, n)
+
 	payload = {
-            'most_frequent_emote': 'some emote',
-            'most_frequent_phrase': 'some phrase',
-            'top_5_emotes': ['one', 'two', 'three', 'four', 'five'],
+            'most_frequent_emote': top_emotes[0],
+            'most_frequent_phrase': phrases,
+            'top_5_emotes': top_emotes,
             'text_summary': 'this is a brief summary of what happened for ' + username + ' ' + channel
             }
 	return json.dumps(payload)
-# DB = Database()
-# a = DB.retrieve_messages_time('arcadiiaaa', '#aaroniidx', '2018-04-23 21:12:37', 5000)
-# print(Stats.get_top_emotes(a, ['hi', 'are', 'you'], 2))
-# phrases, n = Stats.get_top_phrases(a, ['no'], 5)
-# print(phrases, n)
+
